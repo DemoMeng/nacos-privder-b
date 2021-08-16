9883ff9f86c179e832c7450c583c19881bfcdd76
蒙大拿 <977466992@qq.com>
2021-08-16 16:44:39 +0800

    同步a

--- src/main/java/com/mqz/nacos/provider/b/NacosProviderBApplication.java
+++ src/main/java/com/mqz/nacos/provider/b/NacosProviderBApplication.java
@@ -1,13 +1,18 @@
 package com.mqz.nacos.provider.b;
 
+import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, MybatisAutoConfiguration.class})
 @EnableDiscoveryClient
 @ComponentScan(value = {"com.mqz"})//需要扫描到底层包mars项目
+@Configuration
 public class NacosProviderBApplication {
 
     public static void main(String[] args) {
